4fedc93938806eca7760837ebc04eaec360b778e
ORHAN TÜRKMENOĞLU <orhantrkmn749@gmail.com>
2024-07-09 11:19:58 +0300

    user mikro service project eureka server config added.

--- user-service/src/main/java/com/example/user/service/UserServiceApplication.java
+++ user-service/src/main/java/com/example/user/service/UserServiceApplication.java
@@ -2,8 +2,11 @@ package com.example.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+//  ServiceRegistry yerel sunucuyu otomatik olarak kaydeder.
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
