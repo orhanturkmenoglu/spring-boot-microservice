64c0ee0b4d73eb0a3bd660bf3c0e3fd094dfb33c
ORHAN TÜRKMENOĞLU <orhantrkmn749@gmail.com>
2024-07-09 10:42:33 +0300

    eureka server enabled and application.yml config added.

--- service-registry/src/main/java/com/example/service/registry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/example/service/registry/ServiceRegistryApplication.java
@@ -2,8 +2,17 @@ package com.example.service.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+
+/*
+	service registry eureka server active
+	eureka servis başlatıldığı anda bütün çalışan mikro hizmetleri kayıt defterine kayıt edecek.
+	ayrıca aynı port adresinde birden fazla mikro hizmet var ise loadblancer ile yük dengeleme
+	sağlıyacak.
+ */
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
